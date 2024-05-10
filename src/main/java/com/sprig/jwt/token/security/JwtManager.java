85d33d5a90020517f8c700ef5aa0c896a74597b9
deepapasam <144047973+deepapasam@users.noreply.github.com>
2024-05-10 18:26:38 +0530

    token-2

--- src/main/java/com/sprig/jwt/token/security/JwtManager.java
+++ src/main/java/com/sprig/jwt/token/security/JwtManager.java
@@ -1,8 +1,14 @@
 package com.sprig.jwt.token.security;
 
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Component
 public class JwtManager {
+	@GetMapping("/ttt")
+	public String method() {
+		
+		return"hello";
+	}
 
 }
